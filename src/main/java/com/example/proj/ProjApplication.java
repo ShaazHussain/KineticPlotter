0550db5173f5cb345afdff0cc13a54304c6d8aeb
ShaazHussain <95764399+ShaazHussain@users.noreply.github.com>
2024-08-16 16:56:10 +0530

    Update ProjApplication.java

--- src/main/java/com/example/proj/ProjApplication.java
+++ src/main/java/com/example/proj/ProjApplication.java
@@ -10,13 +10,3 @@ public class ProjApplication {
 		SpringApplication.run(ProjApplication.class, args);
 	}
 }
-
-//KineticPlotter is a dynamic web application designed to analyze and visualize chemical reaction kinetics.
-//The application allows users to input concentration and time data directly through the web interface
-//and provides powerful tools for calculating and predicting reaction rates and orders.
-//With interactive plots and an intuitive interface, KineticPlotter helps students, educators, and
-//researchers gain insights into reaction dynamics by visualizing  plots.
-
-//Users can easily switch between different plots with a simple click for
-//better understanding of the chemical processes.
-//The application is built using Java for back-end computation and front-end visualization.
